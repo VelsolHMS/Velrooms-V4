ecdd31d6628ab02390eca7b8b6cb2c21dd2a1449
GuduruUmesh <umesh.velsol@gmail.com>
2019-02-21 10:57:59 +0530

    changes

--- VelRooms/View/Operations/INGUESTHOUSEINFO.xaml.cs
+++ VelRooms/View/Operations/INGUESTHOUSEINFO.xaml.cs
@@ -269,10 +269,5 @@ namespace HMS.View.Operations
                 }
             }
         }
-
-        private void insertpop_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
